5fb5e04363bd212235b48b3d590311599c6f0e04
rizal <rizalsaputrasyarif17@gmail.com>
2021-08-17 05:12:04 -0700

    update desc

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(int argc, char const *argv[]){
     cout << "Cek" <<endl;
-    cout << "Hari Irawan" <<endl;
+    cout << "Rizal Syafutra Syarif" <<endl;
     return 0;
 }
